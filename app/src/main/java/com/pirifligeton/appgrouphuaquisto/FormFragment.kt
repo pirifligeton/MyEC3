d098115c109dd974fa0a934d1d82c1cc262b48ea
pirifligeton <pirifligeton@gmail.com>
2024-03-17 23:51:11 -0500

    continue

--- app/src/main/java/com/pirifligeton/appgrouphuaquisto/FormFragment.kt
+++ app/src/main/java/com/pirifligeton/appgrouphuaquisto/FormFragment.kt
@@ -10,13 +10,21 @@ import android.widget.ArrayAdapter
 
 
 class FormFragment : Fragment() {
+
+    private lateinit var binding : FormFragment
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_form, container, false)
-
+    ArrayAdapter.createFromResource(
+        android.app.Fragment, R.array.spinner1,android.R.layout.simple_spinner_item
+    ).also {
+        adapter ->adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spin
+    }
     }
 
 
