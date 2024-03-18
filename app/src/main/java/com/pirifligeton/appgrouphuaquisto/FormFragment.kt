5bd7a7dfb081abcfaca3b9f6561994af496fabed
pirifligeton <pirifligeton@gmail.com>
2024-03-17 23:01:01 -0500

    basic

---
+++ app/src/main/java/com/pirifligeton/appgrouphuaquisto/FormFragment.kt
@@ -0,0 +1,23 @@
+package com.pirifligeton.appgrouphuaquisto
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+
+
+class FormFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_form, container, false)
+
+    }
+
+
+}
